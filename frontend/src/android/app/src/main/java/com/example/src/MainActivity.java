29bb6608c42ad92c364eaf84be5041739f0949c5
Restyled.io <commits@restyled.io>
2021-10-10 19:57:07 +0000

    Restyled by clang-format

--- frontend/src/android/app/src/main/java/com/example/src/MainActivity.java
+++ frontend/src/android/app/src/main/java/com/example/src/MainActivity.java
@@ -2,5 +2,4 @@ package com.example.src;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
-}
+public class MainActivity extends FlutterActivity {}
