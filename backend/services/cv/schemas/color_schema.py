78fd695b1096e8be215b37bc748a234944c866de
ilyakochankov <ilyakochankov@yandex.ru>
2021-10-22 16:49:00 +0300

    add cv service

---
+++ backend/services/cv/schemas/color_schema.py
@@ -0,0 +1,7 @@
+from marshmallow import Schema
+
+from marshmallow.fields import String
+
+
+class ColorSchema(Schema):
+    color = String()
---
+++ backend/services/cv/schemas/error_schema.py
@@ -0,0 +1,7 @@
+from marshmallow import Schema
+
+from marshmallow.fields import String
+
+
+class ErrorSchema(Schema):
+    error = String()
