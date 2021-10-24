21b300cbd859f18b091a07aef652ca5d7bab6712
aeremina <alena.eremina@intel.com>
2021-10-24 21:20:58 +0300

    deleted trash

--- cv_algs/source/algs.py
+++ cv_algs/source/algs.py
@@ -124,19 +124,6 @@ def findFaceMask(img):
 	
 	return face_mask
 
-def findElements(img):
-	# define a dictionary that maps the indexes of the facial
-	# landmarks to specific face regions
-	FACIAL_LANDMARKS_IDXS = OrderedDict([
-		("mouth", (48, 68)),
-		("right_eyebrow", (17, 22)),
-		("left_eyebrow", (22, 27)),
-		("right_eye", (36, 42)),
-		("left_eye", (42, 48)),
-		("nose", (27, 35)),
-		("jaw", (0, 17))
-	])
-
 # Arguments
 arguments_parser = ArgumentParser()
 arguments_parser.add_argument('-p', '--photo', help='Photo of the face')
